34d3e6b70317878fdd6956f0651fa326e1f5687d
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2025-11-14 15:17:28 -0300

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${merriweather.variable} ${montserrat.variable} antialiased`}
       >
